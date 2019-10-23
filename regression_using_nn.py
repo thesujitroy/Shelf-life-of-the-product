514661ac60acf8ccbd46294103c7540cb308f4a1
Sujit Roy <sujitroy1994@hotmail.com>
2019-10-23 18:31:56 +0100

    regression

---
+++ regression_using_nn.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 23 16:24:43 2019
+
+@author: sb00747428
+"""
+
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.preprocessing import MinMaxScaler
+import pandas as pd
+import numpy as np
