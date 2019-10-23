821a655da39417ced9a8f41f8a9c53e4bedab857
Sujit Roy <sujitroy1994@hotmail.com>
2019-10-23 19:22:46 +0100

    c

--- regression_using_nn.py
+++ regression_using_nn.py
@@ -9,3 +9,40 @@ from sklearn.preprocessing import LabelBinarizer
 from sklearn.preprocessing import MinMaxScaler
 import pandas as pd
 import numpy as np
+from keras.models import Sequential
+from keras.layers.normalization import BatchNormalization
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import MaxPooling2D
+from keras.layers.core import Activation
+from keras.layers.core import Dropout
+from keras.layers.core import Dense
+from keras.layers import Flatten
+from keras.layers import Input
+from keras.models import Model
+
+def dummy_data(df, todummy):
+    for x in todummy:
+        dummies = pd.get_dummies(df[x], prefix = x, dummy_na = False)
+        df= df.drop(x, 1)
+        df = pd.concat([df, dummies], axis=1)
+    return df
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    df =pd.read_excel('data.xlsx')
+    todummy = ['Study_Number']
+    new_data = dummy_data(df, todummy)
+
+    df_miss = new_data.drop(['Sample_ID','Transparent_ Window_in_Package','Sample_Age_(Weeks)'], axis =1)
+    df_final = new_data.drop(['Sample_ID','Transparent_ Window_in_Package'], axis =1)
+
+
+    
